a033d5ee07b2e2a06bab7952cb2e1178425f78e3
Dustie Mercer <d.nichole.mercer@gmail.com>
2021-02-14 13:46:02 -0500

    validate session complete

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ let sequelize = require('./db');
 let user = require('./controllers/usercontroller');
 
 sequelize.sync();
+app.use(require('./middleware/headers'));
 app.use(express.json());
 //all routes below here will JSON-ify, anything above will NOT.
 
 app.use('/user', user);
+app.use(require('./middleware/validate-session'));
+
 
 app.listen(3000, function(){
     console.log("App is listening on port: 3000")
